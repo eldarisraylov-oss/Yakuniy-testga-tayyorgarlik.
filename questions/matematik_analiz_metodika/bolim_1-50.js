78b8cb5fb010957e2c5b7e3cc600706683121f06
eldarisraylov-oss <eldarisraylov@gmail.com>
2026-02-08 23:19:33 +0500

    Add question section for 1–50 savollar

---
+++ questions/matematik_analiz_metodika/bolim_1-50.js
@@ -0,0 +1,556 @@
+window.QUESTION_SECTION = {
+  id: 'bolim_1-50',
+  name: '1–50 savollar',
+  questions: [
+  {
+    "n": 1,
+    "q": "",
+    "options": [
+      "250",
+      "251",
+      "249",
+      "248"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 2,
+    "q": "Quyidagi jumlaning qaysi biri to‘g‘ri?",
+    "options": [
+      "Quyidan chegaralangan ixtiyoriy X sonli to‘plam eng katta elementga ega",
+      "Chegaralangan ixtiyoriy X sonli to‘plam eng katta va eng kichik elementga ega",
+      "Quyidan chegaralangan ixtiyoriy X sonli to‘plam eng kichik elementga ega",
+      "Chegaralangan X sonli to‘plam aniq yuqori va aniq quyi chegaraga ega"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 3,
+    "q": "Quyidagi jumlalarning qaysi biri to‘g‘ri?",
+    "options": [
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 1/59 08.02.2026, 15:02 _",
+      ""
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 4,
+    "q": "",
+    "options": [
+      "a nuqtaning ixtiyoriy atrofida {an} ketma-ketlikning faqat chekli sondagi hadlari mavjud",
+      "a nuqtaning ixtiyoriy atrofida {an} ketma-ketlikning hadlari mavjud emas",
+      "a nuqtaning shunday atrofi topilib, bu atrofga tegishli bo‘lmagan {an} ketma-ketlikning cheksiz ko‘p hadlari mavjud",
+      "a nuqtaning shunday atrofi topilib, bu atrofga {an} ketma-ketlikning faqat chekli sondagi hadlari tegishli"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 5,
+    "q": "",
+    "options": [
+      "1/3 va –3",
+      "1/3 va 0",
+      "1 va 0",
+      "3 va 0"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 6,
+    "q": "",
+    "options": [
+      "a nuqtaning ixtiyoriy atrofida f(x)< b bo‘ladi. https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 2/59 08.02.2026, 15:02 _",
+      "a nuqtaning shunday atrofi mavjud bo‘lib, bunda funksiya manfiy qiymatlar qabul qiladi.",
+      "a nuqtaning ixtiyoriy atrofida funksiya manfiy qiymatlarni qabul qiladi.",
+      "a nuqtaning shunday atrofi mavjud bo‘lib, bunda funksiyaning qiymatlari b dan kichik bo‘ladi."
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 7,
+    "q": "Qanday to‘plamlar chekli to‘plamlar deyiladi?",
+    "options": [
+      "biror radiusi chekli doirada joylashririb bo‘lsa",
+      "ixtiyoriy to‘plam",
+      "Elementlari cheksiz ko‘p bo‘lsa",
+      "Elementlari chekli bo‘lsa"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 8,
+    "q": "",
+    "options": [
+      "{2,3}",
+      "{1,2,3}",
+      "{3,4,5}",
+      "{5,6 }"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 9,
+    "q": "A va V to‘plamlar teng deyiladi agar ………",
+    "options": [
+      "",
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 3/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 10,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 11,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 12,
+    "q": "",
+    "options": [
+      "{6,8,10}",
+      "O",
+      "",
+      "{12 } https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 4/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 13,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 14,
+    "q": "",
+    "options": [
+      "0",
+      "2",
+      "4",
+      "3"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 15,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 5/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 16,
+    "q": "",
+    "options": [
+      "1",
+      "{0}",
+      "{-1;1}.",
+      "1"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 17,
+    "q": "",
+    "options": [
+      "davriy emas",
+      "aniqlab bo‘lmaydi",
+      "Ba’zi oraliqda davriy",
+      "davriy"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 18,
+    "q": "",
+    "options": [
+      "0",
+      "2",
+      "1",
+      "2"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 19,
+    "q": "f(x)=x3 R da",
+    "options": [
+      "xam o‘sadi, xam kamayadi.",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 6/59 08.02.2026, 15:02 _ monoton emas",
+      "o‘suvchi",
+      "kamayuvchi"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 20,
+    "q": "",
+    "options": [
+      "x>0",
+      "R",
+      "D)",
+      ""
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 21,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 22,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 7/59 08.02.2026, 15:02 _",
+      ""
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 23,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 24,
+    "q": "",
+    "options": [
+      "1",
+      "0",
+      "3",
+      "6"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 25,
+    "q": "",
+    "options": [
+      "1",
+      "1",
+      "7",
+      "6 https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 8/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 26,
+    "q": "",
+    "options": [
+      "1",
+      "0",
+      "7",
+      "9"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 27,
+    "q": "",
+    "options": [
+      "5",
+      "2",
+      "1",
+      "4"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 28,
+    "q": "",
+    "options": [
+      "0",
+      "3",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 9/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 2
+  },
+  {
+    "n": 29,
+    "q": "",
+    "options": [
+      "",
+      "4",
+      "8",
+      "1"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 30,
+    "q": "",
+    "options": [
+      "0, 1,-1",
+      "",
+      "1,3,5",
+      "0"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 31,
+    "q": "",
+    "options": [
+      "(-1,1)",
+      "(6,8)",
+      "(6,-1)",
+      "(-5, 6)"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 32,
+    "q": "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 10/59 08.02.2026, 15:02 _",
+    "options": [
+      "(6,8)",
+      "R",
+      "(6,-1)",
+      "(-5, 6)"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 33,
+    "q": "",
+    "options": [
+      "(0,-1)",
+      "R",
+      "(4,8)",
+      "(0,1)"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 34,
+    "q": "",
+    "options": [
+      "(0,-5)",
+      "R",
+      "(0-1)",
+      "(2,5)"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 35,
+    "q": "",
+    "options": [
+      "R",
+      "",
+      "{2,5} https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 11/59 08.02.2026, 15:02 _",
+      "[-4,5]"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 36,
+    "q": "",
+    "options": [
+      "",
+      "(0,-1)",
+      "R",
+      "(1,4)"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 37,
+    "q": "",
+    "options": [
+      "1",
+      "4",
+      "3",
+      "0"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 38,
+    "q": "",
+    "options": [
+      "(0,1)",
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 12/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 39,
+    "q": "",
+    "options": [
+      "R",
+      "0",
+      "4",
+      ""
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 40,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "(-1, 5)",
+      "(1, 4)"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 41,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 13/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 42,
+    "q": "",
+    "options": [
+      "",
+      "",
+      "D)",
+      ""
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 43,
+    "q": "",
+    "options": [
+      "7",
+      "",
+      "2a",
+      "a"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 44,
+    "q": "",
+    "options": [
+      "0",
+      "12",
+      "9",
+      "1 https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 14/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 45,
+    "q": "",
+    "options": [
+      "4",
+      "8",
+      "7",
+      "3"
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 46,
+    "q": "",
+    "options": [
+      "",
+      "n",
+      "0",
+      "m"
+    ],
+    "correctIndex": 0
+  },
+  {
+    "n": 47,
+    "q": "",
+    "options": [
+      "m",
+      "8",
+      "n",
+      ""
+    ],
+    "correctIndex": 3
+  },
+  {
+    "n": 48,
+    "q": "Matematika o‘qitish https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 15/59 08.02.2026, 15:02 _ metodikasi(5)",
+    "options": [
+      "1",
+      "",
+      "",
+      "0"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 49,
+    "q": "",
+    "options": [
+      "1",
+      "3",
+      "0",
+      "4"
+    ],
+    "correctIndex": 1
+  },
+  {
+    "n": 50,
+    "q": "",
+    "options": [
+      "1",
+      "3",
+      "0",
+      "https://yda.uznpu./start_yda_view.php?tester_id=355211102757&rqw=eba5f4e901ad58bdd0d768eec1b87818&lang=&qfn=4 16/59 08.02.2026, 15:02 _"
+    ],
+    "correctIndex": 3
+  }
+]
+};
